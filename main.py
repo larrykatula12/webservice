0e046573498756a4e28cdff1d328ba6a50e53b51
larrykatula12 <porquehaytodos@gmail.com>
2025-09-29 14:23:12 -0700

    d

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def hola():
-    return {"mensaje": "Hola Mundo desde Render"}
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Hola Mundo</title>
+        <style>
+            body {
+                background-color: white;
+                font-family: Arial, sans-serif;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+            h1 {
+                font-size: 3em;
+                color: #222;
+            }
+        </style>
+    </head>
+    <body>
+        <h1> Javier Gabriel López Acosta - 22031006</h1>
+    </body>
+    </html>
+    """
